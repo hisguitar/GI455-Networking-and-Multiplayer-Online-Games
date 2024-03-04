e6934c44d54d2226f98e4f6d3bdd4d65191faa8a
Guitar <wachsirin.sara@gmail.com>
2024-03-04 23:17:09 +0700

    Class8 - Connecting Online 2 [Player Name, Shutting Down Cleanly]

--- WorkshopMultiplayer/Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ WorkshopMultiplayer/Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -8,14 +8,20 @@ using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Text;
+using Unity.Services.Authentication;
 
-public class ClientGameManager : MonoBehaviour
+public class ClientGameManager : IDisposable
 {
     private JoinAllocation allocation;
+    private NetworkClient networkClient;
+
     private const string MenuSceneName = "Menu";
     public async Task<bool> InitAsync()
     {
         await UnityServices.InitializeAsync();
+
+        networkClient = new NetworkClient(NetworkManager.Singleton);
 
         AuthState authState = await AuthenticationWrapper.DoAuth();
 
@@ -43,6 +49,16 @@ public class ClientGameManager : MonoBehaviour
             RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
             transport.SetRelayServerData(relayServerData);
 
+            UserData userData = new UserData
+            {
+                userName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Missing Name"),
+                userAuthId = AuthenticationService.Instance.PlayerId
+            };
+            string payload = JsonUtility.ToJson(userData);
+            byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+
+            NetworkManager.Singleton.NetworkConfig.ConnectionData = payloadBytes;
+
             NetworkManager.Singleton.StartClient();
         }
         catch (Exception e)
@@ -50,5 +66,10 @@ public class ClientGameManager : MonoBehaviour
             Debug.Log(e);
             return;
         }
+    }
+
+    public void Dispose()
+    {
+        networkClient?.Dispose();
     }
 }
